8226c2cb80b1358dc946f602aa660ed781a26e97
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-10-15 11:25:00 +0200

    function for single set volume

---
+++ notebooks/functions.py
@@ -0,0 +1,19 @@
+def calculate_weighted_set_volume(line):
+    """Takes a line like 'Set 1 : 40 kg x 9' and returns volume for that set"""
+    parts = line.split(" x ")
+    # splits string into list: ['Set 3 : 30 kg ', '14\n']
+    sets = parts[0].split(":")
+    # extracts first item in list, splits into new list by colon: ['Set 3 ', ' 30 kg ']
+    weight = sets[1].split(" ")
+    # extracts second item in list, splits new into new list by space: ['', '30', 'kg', '']
+    kilos = weight[1]
+    # assigns second item in list (the number) to variable kilos
+    kilos = float(kilos)
+        # converts kilos from object to float for calculations
+    reps = parts[1].strip()
+    # extracts reps from second item in original list, removes newline character
+    reps = int(reps)
+    # converts reps into an interger for calculations
+    set_volume = kilos * reps
+    # calculates volume for the set
+    return set_volume
